7a1baaca0671a618d239aa3ea6cbe2533868ccf0
KhoiUna <thorwaitson@gmail.com>
2023-06-09 21:13:58 -0500

    modified:   imageprocessing.py

--- imageprocessing.py
+++ imageprocessing.py
@@ -3,33 +3,34 @@
 https://open.kattis.com/problems/imageprocessing?tab=metadata
 '''
 
-imageHeight,imageWidth,kernelHeight,kernelWidth = map(int, input().split())
+image_height,image_width, kernel_height, kernel_width = map(int, input().split())
 
-matrix = []
-for _ in range(imageHeight):
-    matrix.extend([int(i) for i in input().split()])
+original_image = []
+for _ in range(image_height):
+    original_image.extend([int(i) for i in input().split()])
 
 kernel = []
-for _ in range(kernelHeight):
+for _ in range(kernel_height):
     kernel.extend([int(i) for i in input().split()])
 
-resultingImageHeight = imageHeight - kernelHeight + 1
-resultingImageWidth = imageWidth - kernelWidth + 1
 
-for resultingImageHeightIndex in range(resultingImageHeight):
-    resultingImageRow = []
-    for resultingImageWidthIndex in range(resultingImageWidth):
+result_height = image_height - kernel_height + 1
+result_width = image_width - kernel_width + 1
+
+for result_height_i in range(result_height):
+    result_row = []
+    
+    for result_width_i in range(result_width):
         entry = 0
-        for kernelHeightIndex in range(kernelHeight):
-            for kernelWidthIndex in range(kernelWidth):
-                imageHeightIndex = imageWidth * (resultingImageHeightIndex + kernelHeightIndex)
-                imageWidthIndex = resultingImageWidthIndex + kernelWidthIndex
-                imageIndex = imageHeightIndex + imageWidthIndex
-                imageValue = matrix[imageIndex]
 
-                kernelIndex = kernelWidth * (kernelHeight - kernelHeightIndex - 1) + (kernelWidth - kernelWidthIndex - 1)
-                kernelValue = kernel[kernelIndex]
-                entry += imageValue * kernelValue
-        resultingImageRow.append(str(entry))
+        for kernel_height_i in range(kernel_height):
+            for kernel_width_i in range(kernel_width):
+                image_index = image_width * (result_height_i + kernel_height_i) + result_width_i + kernel_width_i
 
-    print(" ".join(resultingImageRow))
+                kernel_index = kernel_width * (kernel_height - kernel_height_i - 1) + kernel_width - kernel_width_i - 1
+
+                entry += original_image[image_index] * kernel[kernel_index]
+
+        result_row.append(str(entry))
+
+    print(' '.join(result_row))
