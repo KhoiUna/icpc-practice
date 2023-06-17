01d3d551c2ac2e8e8209f5444ae18dddbfbef532
KhoiUna <thorwaitson@gmail.com>
2023-06-17 15:11:40 -0500

    modified:   soylent.py

--- soylent.py
+++ soylent.py
@@ -2,10 +2,12 @@
 1.7
 https://open.kattis.com/problems/soylent
 '''
+import math
+
 CALORIE_PER_BOTTLE = 400
 
 n = int(input())
 test_cases = [int(input()) for _ in range(n)]
 
 for v in test_cases:
-    print(v // CALORIE_PER_BOTTLE)
+    print(math.ceil(v / CALORIE_PER_BOTTLE))
