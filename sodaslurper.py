4e25add73b31f4654c97a181afffcfcdf0d053b2
KhoiUna <thorwaitson@gmail.com>
2023-06-17 15:50:29 -0500

    modified:   sodaslurper.py

--- sodaslurper.py
+++ sodaslurper.py
@@ -1,3 +1,20 @@
 '''
-
+1.7
+https://open.kattis.com/problems/sodaslurper
 '''
+
+e,f,c = map(int, input().split())
+
+total_empty = e + f
+
+res = 0 
+
+while total_empty >= c:
+    consume = total_empty // c
+    res += consume
+
+    total_empty %= c
+    total_empty += consume
+
+
+print(res)
